fee98026845187c11472162eeeb6f441dc1efc80
dokhanhvu <igneel.vv@gmail.com>
2015-12-08 00:49:58 +0700

    them project

---
+++ XML/XML/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Đường dẫn thư mục chứa file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(385, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Sach",
+            "DienThoai",
+            "MyPham",
+            "Loa",
+            "XeDap",
+            "SinhVien",
+            "MonHoc",
+            "KhoaHoc",
+            "CauThu",
+            "Ruou"});
+            this.comboBox1.Location = new System.Drawing.Point(180, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(259, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.Selection);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(180, 125);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(259, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedValueChanged += new System.EventHandler(this.Selection2);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Chọn Nhóm Đối Tượng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Chọn Đối Tượng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CHƯƠNG TRÌNH ĐỌC FILE XML";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ XML/XML/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace XML
+{
+    public partial class Form1 : Form
+    {
+        string[] filePahts = new string[100];
+        string filePahts2;
+        List<string> AtributesName = new List<string>();
+        List<List<string>> Value = new List<List<string>>();
+        Label[] lbl = new Label[6];
+        TextBox[] tbx = new TextBox[6];
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 6; i++)
+            {
+                lbl[i] = new Label();
+                tbx[i] = new TextBox();
+                lbl[i].Hide();
+                tbx[i].Hide();
+            }
+            comboBox1.Hide();
+            comboBox2.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+                filePahts = System.IO.Directory.GetFiles(textBox1.Text, "*.xml", SearchOption.TopDirectoryOnly);
+            }
+            if (filePahts.Length == 0)
+            {
+                MessageBox.Show("Khong tim thay file XML", "Loi");
+            }
+            else
+            {
+                comboBox1.Show();
+                comboBox2.Show();
+            }
+        }
+
+        private void Selection(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            AtributesName.Clear();
+            Value.Clear();
+
+            for (int i = 0; i < filePahts.Length; i++)
+            {
+                if (filePahts[i].Contains(comboBox1.Text))
+                {
+                    filePahts2 = filePahts[i];
+                }
+            }
+
+            XmlDocument reader = new XmlDocument();
+            reader.Load(filePahts2);
+
+            XmlElement elm = reader.DocumentElement;
+            int nChild = elm.ChildNodes.Count;
+            int nAtru = elm.ChildNodes[0].Attributes.Count;
+
+            for (int i = 0; i < nAtru; i++)
+            {
+                AtributesName.Add(elm.ChildNodes[0].Attributes[i].Name);
+            }
+
+            for (int i = 0; i < nChild; i++)
+            {
+                List<string> tmp = new List<string>();
+                for (int j = 0; j < nAtru; j++)
+                {
+                    tmp.Add(elm.ChildNodes[i].Attributes[j].InnerText);
+                }
+                Value.Add(tmp);
+            }
+
+            for (int i = 0; i < nChild; i++)
+            {
+                comboBox2.Items.Add(Value[i][1]);
+            }
+        }
+
+        private void Selection2(object sender, EventArgs e)
+        {
+            int n = AtributesName.Count;
+            int m = new int();
+            for (int i = 0; i < Value.Count; i++)
+            {
+                if (Value[i][1] == comboBox2.Text)
+                    m = i;
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (i < n)
+                {
+                    lbl[i].Show();
+                    tbx[i].Show();
+                    lbl[i].Name = AtributesName[i];
+                    lbl[i].Text = AtributesName[i];
+                    tbx[i].Text = Value[m][i];
+                    lbl[i].Location = new System.Drawing.Point(13, 200 + i * 40); // vi tri xuat hien
+                    tbx[i].Location = new System.Drawing.Point(150, 200 + i * 40);
+                    lbl[i].Size = new System.Drawing.Size(100, 20);  // kich thuoc
+                    tbx[i].Size = new System.Drawing.Size(300, 40);  // kich thuoc
+                    this.Controls.Add(lbl[i]);// them controls vaof Form1
+                    this.Controls.Add(tbx[i]);
+                }
+                else
+                {
+                    lbl[i].Hide();
+                    tbx[i].Hide();
+                }
+            }
+        }
+    }
+}
